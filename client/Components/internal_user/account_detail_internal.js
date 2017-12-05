ed178cb901fcc12d589a15922e9dbc5a35cdbb71
PhuongBlueplate <phuong.le@blueplate.co>
2017-12-05 16:52:59 +0700

    update login

--- client/Components/internal_user/account_detail_internal.js
+++ client/Components/internal_user/account_detail_internal.js
@@ -4,7 +4,3 @@ import { Template } from 'meteor/templating';
 import { Blaze } from 'meteor/blaze';
 import { FilesCollection } from 'meteor/ostrio:files';
 import { navbar_find_by } from '/imports/functions/find_by.js';
-
-Template.account_detail_internal.helper(function(){
-
-});
--- client/Components/iu_login/iu_login.js
+++ client/Components/iu_login/iu_login.js
@@ -1,44 +1,26 @@
-import { Accounts } from 'meteor/accounts-base'
+import { Meteor } from 'meteor/meteor';
 import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
-import { Template } from 'meteor/templating';
-import { Blaze } from 'meteor/blaze';
-import { FilesCollection } from 'meteor/ostrio:files';
-import { navbar_find_by } from '/imports/functions/find_by.js';
 
-// Template.iu_login.events({
-//   'submit form': function(event) {
-//     event.preventDefault();
-//     var emailVar = event.target.loginEmail.value;
-//     var passwordVar = event.target.loginPassword.value;
-//
-//     Meteor.loginWithPassword(emailVar, passwordVar);
-//   }
-// });
-
-Template.Login.events({
-    'submit #login': function(event, template) {
-      // 1. Collect the username and password from the form
-      var username = template.find('#login-username').value,
-          password = template.find('#login-password').value;
-
-      // 2. Attempt to login.
-      Meteor.loginWithPassword(username, password, function(error) {
-          // 3. Handle the response
-          if (Meteor.user()) {
-              // Redirect the user to where they're loggin into. Here, Router.go uses
-              // the iron:router package.
-              Router.go('Internal User');
-          } else {
-              // If no user resulted from the attempt, an error variable will be available
-              // in this callback. We can output the error to the user here.
-              var message = "There was an error logging in: <strong>" + error.reason + "</strong>";
-
-              template.find('#form-messages').html(message);
-          }
-
-          return;
+if(Meteor.isClient){
+  Template.iu_register.events({
+    'submit form': function(event, template){
+      event.preventDefault();
+      var emailVal = template.find('#register-email').value;
+      var passVal = template.find('#register-password').value;
+      Accounts.createUser({
+        email: emailVal,
+        password: passVal
       });
-
-      return false;
+      FlowRouter.go('AccountIU');
     }
-});
+  });
+
+  Template.iu_login.events({
+    'submit form': function(event, template){
+      event.preventDefault();
+      var emailVal = template.find('#login-email').value;
+      var passVal = template.find('#login-password').value;
+      Meteor.loginWithPassword(emailVal, passVal);
+    }
+  });
+}
