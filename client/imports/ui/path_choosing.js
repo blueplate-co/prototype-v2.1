315ca59a6198a2285890e2b641fedb3a11acacf9
blueplate-michaellin <michael.lin@blueplate.co>
2018-03-21 00:07:24 +0800

    complete path choosing react component

--- client/imports/ui/path_choosing.js
+++ client/imports/ui/path_choosing.js
@@ -3,20 +3,45 @@ import React, { Component } from 'react';
 export default class PathOption extends Component {
   constructor(props) {
     super(props);
+  }
 
+  get_path() {
+    return [
+      {
+        link: "https://s3-ap-southeast-1.amazonaws.com/blueplate-images/icons/BeHomecook.svg",
+        name: "create your first dish",
+      }, {
+        link: "https://s3-ap-southeast-1.amazonaws.com/blueplate-images/icons/ProfilePreview.svg",
+        name: "preview kitchen profile",
+      }, {
+        link: "https://s3-ap-southeast-1.amazonaws.com/blueplate-images/icons/Dashboard.svg",
+        name: "go to kitchen dashboard",
+      }
+    ];
+  }
+
+  render_path = () => {
+    return this.get_path().map((path) => {
+      return (
+        <div className='col xl4 l4 m12 s12'>
+          <div className='card z-depth-0 path_card'>
+            <div className='card-content'>
+              <a>
+                <img src={path.link} className="path_icons"/>
+                <h6 className='icon_titles bp-blue-text center-align' id='create_first_dish'>{path.name}</h6>
+              </a>
+            </div>
+          </div>
+        </div>
+      )
+    })
   }
 
   render() {
     return (
-      <div className="container">
-        <div className='col l4 m12 s12'>
-          <h2 id='create_first_dish'>create your first dish</h2>
-        </div>
-        <div className='col l4 m12 s12'>
-          <h2 id='preview_kitchen_profile'>preview kitchen profile</h2>
-        </div>
-        <div className='col l4 m12 s12'>
-          <h2 id='divert_showroom'>browser around</h2>
+      <div className="path_container valign-wrapper">
+        <div className="row center-align">
+          {this.render_path()}
         </div>
       </div>
     );
