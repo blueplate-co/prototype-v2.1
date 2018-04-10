c3f9d12d4b79d6b33fcb7f659600ed7b147ba3c2
blueplate-michaellin <michael.lin@blueplate.co>
2018-04-10 08:56:39 +0800

    total view component
    Finish totalview component

--- client/imports/ui/total_views.js
+++ client/imports/ui/total_views.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 
-const TotalViews = () => {
-  const getViews = () => {
+export default class TotalViews extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: 0
+    }
+  }
+
+  componentDidMount() {
     Meteor.call('total.views', (error, result) => {
-      console.log(error);
-      console.log(result);
-      return result;
+      return this.setState({view:result});
     })
   }
 
-  return (
-    <div className = 'totalViews_wrapper'>
-      <p>total views:</p>
-      <h1>{getViews}</h1>
-    </div>
-  );
-
+  render() {
+    return(
+        <div className = 'valign-wrapper' style = {{ height: '100%' }}>
+        <div className = 'col l12 m12 s12'>
+            <p>total views:</p>
+            <h1>{this.state.view}</h1>
+        </div>
+        </div>
+    );
+  }
 }
-
-export default TotalViews;
