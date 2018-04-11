c821e32c7dc4719cebd72a789f7d697095fb3cc8
tanpham <tan.pham@blueplate.co>
2018-04-11 13:32:34 +0700

    modify shopping cart and  add routes to select payment method

---
+++ client/imports/ui/payment.js
@@ -0,0 +1,172 @@
+import React, { Component } from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
+
+// Shopping cart component
+class Payment extends Component {
+    constructor(props) {
+        super(props);
+        this.renderPaymentSelect = this.renderPaymentSelect.bind(this);
+        this.choosePayment = this.choosePayment.bind(this);
+        this.validationCardAndCharge = this.validationCardAndCharge.bind(this);
+        this.state = {
+            payment: ""
+        }
+    }
+
+    // componentDidUpdate() {
+    //     if (!Session.get('product')) {
+    //         Materialize.toast('Please complete your order before.', 'rounded bp-green');
+    //         FlowRouter.go('/shopping_cart');
+    //         return true;
+    //     }
+    // }
+
+    choosePayment(payment) {
+        if (payment == 'credits') {
+
+        } else {
+            this.setState({
+                payment: payment
+            });
+        }
+    }
+
+    validationCardAndCharge() {
+        ccNum = $('#card_no').val();
+        cvc = $('#cvc_no').val();
+        expMo = $('#exp_month').val();
+        expYr = $('#exp_year').val();
+
+        // validation card info
+        Stripe.card.createToken({
+            number: ccNum,
+            cvc: cvc,
+            exp_month: expMo,
+            exp_year: expYr,
+        }, function(status, response) {
+            if (response.error) {
+                Materialize.toast(response.error.message, 'rounded bp-green');
+            } else {
+                var StripeToken = response.id;
+                var transaction_no = 1;
+                //- add each every product into order collection
+                var shoppingCart = Shopping_cart.find({ buyer_id: Meteor.userId() }).fetch();
+                shoppingCart.map(function(item, index) {
+                    var kitchenOrderInfo;
+                    //- get information order which user set in previous step
+                    for (var i = 0 ; i < Session.get('product').length; i++) {
+                        if (item.seller_id == Session.get('product')[i].id) {
+                            kitchenOrderInfo = Session.get('product')[i];
+                        }
+                    }
+
+                    var transaction = Transactions.findOne({ 'buyer_id': Meteor.userId(), 'seller_id': kitchenOrderInfo.id }, { sort: { transaction_no: -1 } });
+                    if (transaction) {
+                        transaction_no = parseInt(transaction.transaction_no) + 1
+                    }
+
+                    Meteor.call('order_record.insert', transaction_no, Meteor.userId(), item.seller_id, item.product_id, item.quantity, item.total_price_per_dish, kitchenOrderInfo.address, kitchenOrderInfo.service, kitchenOrderInfo.timeStamp, StripeToken, function (err, response) {
+                        if (err) {
+                            Materialize.toast('Oops! Error occur. Please try again.' + err, 4000, 'rounded bp-green');
+                        } else {
+                            Meteor.call('shopping_cart.remove', item._id)
+                            Meteor.call('notification.place_order', kitchenOrderInfo.id, Meteor.userId(), item.product_id, item.quantity)
+                            Session.clear;
+                            FlowRouter.go('/orders_tracking');
+                        }
+                    })
+                })
+            }
+        })
+    }
+
+    renderPayment(payment) {
+        switch (payment) {
+            case 'credits':
+                return (
+                    <h1>No support now</h1>
+                )
+                break;
+            case 'credit-card':
+                return (
+                    <div className="container" style={{ paddingTop: '40px' }} onKeyDown = {
+                            (event) => {
+                                if (event.keyCode == 13) {
+                                    this.validationCardAndCharge()
+                                }
+                            }
+                        }>
+                        <div className="row">
+                            <h2>Your card details</h2>
+                        </div>
+                        <div className="row">
+                            <div className="col s12 m12 l12 xl12">
+                                <input type="text" placeholder="card holder name" />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col s12 m12 l12 xl12">
+                                <input id="card_no" type="number" placeholder="your credit card number" />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col s4 m4 l4 xl4">
+                                <input id="exp_month" type="number" maxLength="2" size="2" placeholder="MM" />
+                            </div>
+                            <div className="col s4 m4 l4 xl4">
+                                <input id="exp_year" type="number" maxLength="2" size="2" placeholder="YY" />
+                            </div>
+                            <div className="col s4 m4 l4 xl4">
+                                <input id="cvc_no" type="number" maxLength="3" size="3" placeholder="CVC" />
+                            </div>
+                        </div>
+                        <div className="row text-center">
+                            <div className="col s12 m6 offset-m3 l6 offset-l3 xl6 offset-xl3">
+                                <button className="btn" onClick={ () => this.validationCardAndCharge() } style={{ marginTop: '30px' }}>Next</button>
+                            </div>
+                        </div>
+                    </div>
+                )
+                break;
+        }
+    }
+
+    renderPaymentSelect() {
+        return (
+            <div className="container" style={{ marginTop: '50px' }}>
+                <div className="row">
+                    <div className="col s12 m6 l6 xl6">
+                        <div className="payment-wrapper" onClick={ () => this.choosePayment('credits') }>
+                            <img src="https://s3-ap-southeast-1.amazonaws.com/blueplate-images/icons/card.svg" />
+                            <span className="text">Credits</span>
+                        </div>
+                    </div>
+                    <div className="col s12 m6 l6 xl6">
+                        <div className="payment-wrapper" onClick={ () => this.choosePayment('credit-card') }>
+                            <img src="https://s3-ap-southeast-1.amazonaws.com/blueplate-images/icons/credits.svg" />
+                            <span className="text">Credit card</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    render() {
+        return (
+            (this.state.payment == "") ?
+                this.renderPaymentSelect()
+            :   this.renderPayment(this.state.payment)
+        )
+    }
+}
+
+export default withTracker(props => {
+    const handle = Meteor.subscribe('getUserShoppingCart');
+    return {
+        currentUser: Meteor.user(),
+        listLoading: !handle.ready(),
+        shoppingCart: Shopping_cart.find({ buyer_id: Meteor.userId() }).fetch(),
+    };
+})(Payment);
--- client/imports/ui/shopping_cart.js
+++ client/imports/ui/shopping_cart.js
@@ -3,6 +3,14 @@ import { withTracker } from 'meteor/react-meteor-data';
 
 import ProgressiveImages from './progressive_image';
 import ChefAvatar from '../ui/chef_avatar';
+import TimePicker from 'rc-time-picker';
+import moment from 'moment';
+import { Session } from 'meteor/session';
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
+
+//- empty cart store global in this page
+window.globalCart = [];
+// var kitchen = { id: item, service: "", date: "", time: "", timeStamp: "", address: "" };
 
 // Shopping cart component
 class ShoppingCart extends Component {
@@ -10,11 +18,21 @@ class ShoppingCart extends Component {
         super(props);
         this.renderListKitchen = this.renderListKitchen.bind(this);
         this.renderKitchen = this.renderKitchen.bind(this);
+        this.handleChangeServiceOption = this.handleChangeServiceOption.bind(this);
     }
 
     // remove item in shopping cart
     removeItem(id) {
-
+        //- check again, if all products of kitchen are removed. update the global cart
+        let unique = [...new Set(this.props.shoppingCart.map(item => item.seller_id))];
+        globalCart = [];
+        for (var i = 0; i < globalCart.length; i++) {
+            for (var j = 0; j < unique.length; j++) {
+                if (globalCart[i].id !== unique[j]) {
+                    globalCart.splice(i, 1);
+                }
+            }
+        }
         Meteor.call('shopping_cart.remove', id);
     }
 
@@ -53,6 +71,125 @@ class ShoppingCart extends Component {
         )
     }
 
+    // when change service option for kitchen
+    handleChangeServiceOption(event, seller_id) {
+        for (var i = 0; i < globalCart.length; i++) {
+            if (globalCart[i].id == seller_id) {
+                globalCart[i].service = event.target.value;
+                if (event.target.value == "Dine-in") {
+                    var kitchenAddress = Kitchen_details.findOne({ "user_id": seller_id }).kitchen_address;
+                    globalCart[i].address = kitchenAddress;
+                    var commaIndex = kitchenAddress.indexOf(",");
+                    var shortAddress = kitchenAddress.substring(commaIndex + 1, kitchenAddress.length).trim();
+                    $("#label_" + seller_id).text('Your dishes will be ready at chef’s kitchen at...');
+                    $("#address_" + seller_id).val(shortAddress);
+                    $("#address_" + seller_id).attr('disabled', !$(this).attr('checked'));
+                } else if (event.target.value == "Delivery") {
+                    $("#label_" + seller_id).text('Your dishes will be delivered to...');
+                    $("#address_" + seller_id).removeAttr('disabled');
+                } else {
+                    $("#label_" + seller_id).text('Your dishes will be ready at chef’s kitchen at...');
+                    $("#address_" + seller_id).removeAttr('disabled');
+                }
+            }
+        }
+    }
+
+    // when change address for kitchen
+    handleChangeAddress(event, seller_id) {
+        for (var i = 0; i < globalCart.length; i++) {
+            if (globalCart[i].id == seller_id) {
+                globalCart[i].address = $("#address_" + seller_id).val();
+            }
+        }
+    }
+
+    // when change time for kitchen
+    handleChangeTime(value, seller_id) {
+        for (var i = 0; i < globalCart.length; i++) {
+            if (globalCart[i].id == seller_id) {
+                globalCart[i].time = value.format('HH:mm');
+                // use old code from Michael to store preferred_time_ms
+                var yyyy = globalCart[i].date[6] + globalCart[i].date[7] + globalCart[i].date[8] + globalCart[i].date[9]
+                var mm = parseInt(globalCart[i].date[3] + globalCart[i].date[4]) - 1
+                var dd = globalCart[i].date[0] + globalCart[i].date[1]
+                var hh = globalCart[i].time[0] + globalCart[i].time[1]
+                var min = globalCart[i].time[3] + globalCart[i].time[4]
+                serve_date = new Date(yyyy, mm, dd, hh, min)
+                serve_date = Date.parse(serve_date);
+                globalCart[i].timeStamp = serve_date;
+            }
+
+            var readytime = [];
+            this.props.shoppingCart.map(function(item, index){
+                var specificReadytime = moment().add(item.ready_time, 'mins').valueOf();
+                readytime.push(specificReadytime);
+            });
+
+            for (var i = 0; i < readytime.length; i++) {
+                if (globalCart[i].timeStamp < readytime[i]) {
+                    Materialize.toast('Preferred Ready Time must be later than the Latest Ready Time', 'rounded bp-green');
+                    globalCart[i].timeStamp = '';
+                    return true;
+                }
+            }
+
+        }
+    }
+
+    // when change date for kitchen
+    handleChangeDate(event, seller_id) {
+        for (var i = 0; i < globalCart.length; i++) {
+            if (globalCart[i].id == seller_id) {
+                globalCart[i].date = moment(event.target.value).format('DD/MM/YYYY');
+                // use old code from Michael to store preferred_time_ms
+                var yyyy = globalCart[i].date[6] + globalCart[i].date[7] + globalCart[i].date[8] + globalCart[i].date[9]
+                var mm = parseInt(globalCart[i].date[3] + globalCart[i].date[4]) - 1
+                var dd = globalCart[i].date[0] + globalCart[i].date[1]
+                var hh = globalCart[i].time[0] + globalCart[i].time[1]
+                var min = globalCart[i].time[3] + globalCart[i].time[4]
+                serve_date = new Date(yyyy, mm, dd, hh, min);
+                serve_date = Date.parse(serve_date);
+                globalCart[i].timeStamp = serve_date;
+            }
+
+            debugger
+            var readytime = [];
+            this.props.shoppingCart.map(function(item, index){
+                var specificReadytime = moment().add(item.ready_time, 'mins').valueOf();
+                readytime.push(specificReadytime);
+            });
+
+            for (var i = 0; i < readytime.length; i++) {
+                if (globalCart[i].timeStamp < readytime[i]) {
+                    Materialize.toast('Preferred Ready Time must be later than the Latest Ready Time', 'rounded bp-green');
+                    globalCart[i].timeStamp = '';
+                    return true;
+                }
+            }
+        }
+    }
+
+    // when user click checkout button
+    handleCheckout() {
+        var valid = true;
+        globalCart.forEach((item, index) => {
+            item.address = $('#address_' + item.id).val();
+        });
+        globalCart.forEach((item, index) => {
+            for( var key in item ) {
+                if (item[key] == '') {
+                    Materialize.toast('Oops! Please complete all information in your order.', 'rounded bp-green');
+                    valid = false;
+                }
+            }
+            if (valid) {
+                Session.set('product', globalCart);
+                FlowRouter.go('/payment');
+            }
+        })
+    }
+
     renderSingleProduct(product) {
         return product.map((item, index) => {
             var type = "dish";
@@ -66,10 +203,10 @@ class ShoppingCart extends Component {
                 (type == "dish") ?
                 (
                     <div key={index} className="row detail-product">
-                        <div className="col s1 m1 l1 xl1">
+                        <div className="col s3 m3 l1 xl1">
                             <div className="detail-thumbnail" style={{ backgroundImage: "url(" + detail.meta.large + ")" }} ></div>
                         </div>
-                        <div className="col s11 m11 l11 xl11 product-info">
+                        <div className="col s9 m9 l11 xl11 product-info">
                             <span className="fa fa-times remove-item" onClick={ () => this.removeItem(item._id) }></span>
                             <span className="detail-title">{ detail.dish_name }</span>
                             <span className="detail-price">HK${ detail.dish_selling_price }</span>
@@ -102,18 +239,54 @@ class ShoppingCart extends Component {
         });
     }
 
+    renderServingOption(seller_id) {
+        var serviceOption = Kitchen_details.find({ user_id: seller_id }).fetch()[0].serving_option;
+
+        return serviceOption.map(function(item, index){
+            return <option key={index} value={item}>{item}</option>
+        })
+    }
+
     renderKitchen(seller_id, index) {
         var product = Shopping_cart.find({ seller_id: seller_id }).fetch();
         var sellerName = product[0].seller_name;
         var subtotal = 0;
+        var curr = new Date();
+        curr.setDate(curr.getDate());
+        var date = curr.toISOString().substr(0,10);
         for (var i = 0; i < product.length; i++) {
             subtotal += parseInt(product[i].total_price_per_dish);
         }
         return (
-            <div className="row" key={index}>
+            <div key={index}>
                 <div className="row kitchen-name">
                     { sellerName }'s kitchen
                     <ChefAvatar userId={seller_id} />
+                </div>
+                <div className="row">
+                    <div className="col s4">
+                        <select className="browser-default no-border" onChange={(event) => this.handleChangeServiceOption(event, seller_id)} >
+                            <option value="" disabled selected>Service Options</option>
+                            { this.renderServingOption(seller_id) }
+                        </select>
+                    </div>
+                    <div className="col s4">
+                        <input defaultValue={ date } id="date" type="date" placeholder="date" onChange={(event) => this.handleChangeDate(event, seller_id)} />
+                    </div>
+                    <div className="col s4 no-background">
+                        <TimePicker
+                            showSecond={false}
+                            className=""
+                            defaultValue={moment().add(1, 'hours')}
+                            onChange={(value) => this.handleChangeTime(value, seller_id)}
+                        />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col s12">
+                        <label id={"label_" + seller_id}></label>
+                        <input id={"address_" + seller_id} className="address" placeholder="address" type="text" onChange={(event) => this.handleChangeAddress(event, seller_id)} />
+                    </div>
                 </div>
                 { this.renderSingleProduct(product) }
                 <div className="row subtotal">
@@ -126,10 +299,14 @@ class ShoppingCart extends Component {
 
     renderListKitchen() {
         //- get unique seller_id
+        globalCart = [];
         let unique = [...new Set(this.props.shoppingCart.map(item => item.seller_id))];
         if (unique.length > 0) {
             return (
                 unique.map((item, index) => {
+                    //- create new object to store all information about this kitchen
+                    var kitchen = { id: item, service: "", date: moment(event.target.value).format('DD/MM/YYYY'), time: moment().add(1, 'hours').format('HH:mm'), timeStamp: moment().add(moment.duration(1, 'hours')).valueOf(), address: "" };
+                    globalCart.push(kitchen);
                     return this.renderKitchen(item, index);
                 })
             )
@@ -138,11 +315,22 @@ class ShoppingCart extends Component {
         }
     }
 
+    componentWillUpdate() {
+        //- run google places autocomplete with timeout to make sure HTML is rendered
+        setTimeout(() => {
+            var input = document.getElementsByClassName('address');
+            for (i = 0; i < input.length; i++) {
+                var autocomplete = new google.maps.places.Autocomplete(input[i]);
+            }
+        }, 500);
+    }
+
     render() {
         var total = 0;
         for (var i = 0; i < this.props.shoppingCart.length; i++ ) {
             total += parseInt(this.props.shoppingCart[i].total_price_per_dish);
         }
+        Session.set('product', '');
         return (
             <div className="container">
                 <h3>Order summary</h3>
@@ -155,6 +343,9 @@ class ShoppingCart extends Component {
                 <div className="row total">
                     <div className="col s4 text-left">Total</div>
                     <div className="col s8 text-right">HK${ total }</div>
+                </div>
+                <div className="row text-center">
+                    <button className="btn checkout" disabled={this.props.shoppingCart.length == 0} onClick={ () => this.handleCheckout() } >Checkout</button>
                 </div>
             </div>
         )
