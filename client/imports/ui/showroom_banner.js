5675d0eab6342ff1eff67b28c5f5943d87fbdd0e
blueplate-michaellin <michael.lin@blueplate.co>
2018-04-16 23:13:37 +0800

    change banner content

--- client/imports/ui/showroom_banner.js
+++ client/imports/ui/showroom_banner.js
@@ -5,7 +5,7 @@ export default class ShowroomBanner extends Component {
     return(
       <div className="col l12 m12 s12 showroom_banner">
         <h2 className = "white-text">Are you ready?</h2>
-        <h5 className = "white-text">beta launch on 1 May 2018</h5>
+        <h5 className = "white-text">beta launch in May 2018</h5>
         <img src = "https://s3-ap-southeast-1.amazonaws.com/blueplate-images/icons/logo.svg" className = "right banner_logo" />
       </div>
     )
