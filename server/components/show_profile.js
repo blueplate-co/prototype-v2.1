228ea64447055f5b36fef84d4ab65b36c4ccd2d9
blueplate-michaellin <michael.lin@blueplate.co>
2018-04-19 20:54:28 +0800

    fixes

--- server/components/show_profile.js
+++ server/components/show_profile.js
@@ -3,5 +3,8 @@ Meteor.methods({
     var dishes_likes = DishesLikes.find({user_id: user_id}).fetch().length;
     var menus_likes = MenusLikes.find({user_id: user_id}).fetch().length;
     return dishes_likes + menus_likes;
+  },
+  'kitchen_tried.get' (user_id) {
+    return Order_record.find({seller_id: user_id}).fetch().length
   }
 })
