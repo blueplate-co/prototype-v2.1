2b31708c28afac6982e3d6c29af4ba49068a4541
Tan Pham <tan.pham@blueplate.co>
2018-06-12 14:33:45 +0700

    block permission for blueplate email

--- client/Components/login_modal/login_modal.js
+++ client/Components/login_modal/login_modal.js
@@ -44,6 +44,12 @@ Template.login_modal.events({
       var email = $('#login_email').val().trim();
       var password = $('#login_password').val().trim();
 
+      if (email.indexOf('@blueplate.co') == -1) {
+        // login with outer email from blueplate
+        Bert.alert('Only account login with Blueplate email can be logged in!' , 'danger', 'fixed-top');
+        return false;
+      }
+
 
       //- basic validation
       var isOK = false;
