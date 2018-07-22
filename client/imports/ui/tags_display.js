aaccd2309134aa6618aae1acd4ab53bce83e67fd
blueplate-michaellin <michael.lin@blueplate.co>
2018-07-22 23:20:24 +0800

    add color to tag

--- client/imports/ui/tags_display.js
+++ client/imports/ui/tags_display.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Meteor } from 'meteor/meteor';
+import { Session } from 'meteor/session';
 
 export default class TagsDisplay extends React.Component {
   constructor(props) {
@@ -9,14 +10,9 @@ export default class TagsDisplay extends React.Component {
     this.state = {
       tags: [],
       move: 0,
+      color: []
     }
     this.style = {
-      tag: {
-        display: 'inline-block',
-        overflow: 'visible',
-        fontSize: '16px',
-        zIndex: '1000',
-      },
       tagWrapper: {
         paddingTop: '12px',
         position: 'absolute',
@@ -29,6 +25,7 @@ export default class TagsDisplay extends React.Component {
         transition: 'top 0.3s',
         verticalAlign: 'middle',
         maxHeight: '80px',
+        cursor: 'pointer',
       },
       chevronLeft: {
         display: 'inline-block',
@@ -47,6 +44,7 @@ export default class TagsDisplay extends React.Component {
         overflowX: 'hidden',
         position: 'relative',
         display: 'inline-block',
+        cursor: 'pointer',
       }
     }
   }
@@ -62,8 +60,35 @@ export default class TagsDisplay extends React.Component {
 
   listTags() {
     return this.state.tags.map((item, index) => {
+      const color = [
+        {
+          name: "blue",
+          code: "#56AACD"
+        },
+        {
+          name: "orange",
+          code: "#EFAB1E"
+        },
+        {
+          name: "green",
+          code: "#B1DBBE"
+        },
+        {
+          name: "red",
+          code: "#EB5F55"
+        }
+      ];
+      const randomNumber = Math.floor(Math.random() * 4);
+      const tagStyle = {
+        display: 'inline-block',
+        overflow: 'visible',
+        fontSize: '16px',
+        zIndex: '1000',
+        backgroundColor: `${color[randomNumber].code}`,
+        cursor: 'pointer',
+      };
       return (
-          <a key = {index} className = "chip bp-red" style = {this.style.tag} href="">{item.tag_name}</a>
+          <a key = {index} className = "chip" style = {tagStyle} onClick={()=> Session.set('tag_selected', item)}>{item.tag_name}</a>
       )
     })
   }
@@ -91,13 +116,16 @@ export default class TagsDisplay extends React.Component {
         whiteSpace: 'nowrap',
         transform: 'translateX(' + this.state.move + 'px)',
         transition: 'transform 200ms ease-in-out',
-        zIndex: '-1000'
       },
     }
     return (
       <div style = {this.style.tagWrapper}>
         <h6>Can't decide? Here's some extra help:</h6>
-          <button className = 'btn-floating white z-depth-0 waves-effect waves-red' style = {this.style.chevronLeft} onClick={this.handleMoveLeft}>
+          <button
+            className = 'btn-floating transparent z-depth-0 waves-effect waves-red hide-on-small-only'
+            style = {this.style.chevronLeft}
+            onClick={this.handleMoveLeft}
+          >
             <i className="black-text medium material-icons">chevron_left</i>
           </button>
           <span style = {this.style.mask}>
@@ -105,7 +133,12 @@ export default class TagsDisplay extends React.Component {
               {this.listTags()}
             </span>
           </span>
-          <button className = 'btn-floating white z-depth-0 waves-effect waves-red' style = {this.style.chevronRight} onClick={this.handleMoveRight}>
+          <button
+            className = 'btn-floating transparent z-depth-0 waves-effect waves-red hide-on-small-only'
+            style = {this.style.chevronRight}
+            onClick={this.handleMoveRight}
+            disabled={(this.state.move == 0) ? true : false}
+          >
             <i className="black-text medium material-icons">chevron_right</i>
           </button>
       </div>
