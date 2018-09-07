b86f98ca9ad3bcad551412e217b361101f3b7bdf
ThePhan <the.phan@blueplate.co>
2018-09-07 16:36:44 +0700

    [bps-106] Update code Show status/ online,offline next to the card

--- client/imports/ui/self_dish_list.js
+++ client/imports/ui/self_dish_list.js
@@ -65,8 +65,8 @@ class SelfDishList extends Component {
           </div>
 
           <div className="row no-margin">
-            <div className="col s12 l6 m6 dish-price no-padding text-left">$ { item.dish_selling_price }</div>
-            <div className="col s12 l6 m6 dish-price no-padding text-right">
+            <div className="col s6 l6 m6 dish-price no-padding text-left">$ { item.dish_selling_price }</div>
+            <div className="col s6 l6 m6 dish-price no-padding text-right">
               <DishStatus status={item.online_status}/>
             </div>
           </div>
