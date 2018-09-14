8ac65d39bc339aad6337e0bf42ccca7b39355740
Tan Pham <tan.pham@blueplate.co>
2018-09-14 16:09:57 +0700

    remove all promotion dishes in promotion program

--- imports/functions/common/promotion_common.js
+++ imports/functions/common/promotion_common.js
@@ -3,38 +3,38 @@
  * 
  * @param {*} dish_id dish id
  */
+const dish_list = [
+    // {
+    //     id: 'gTgGQ8rF8qo4thFnB',
+    //     amount: 0.5
+    // },
+    // {
+    //     id: 'jc4eWDq66ghmogQFC',
+    //     amount: 0.5
+    // },
+    // {
+    //     id: '5D8osuvKbfpCmy9dr',
+    //     amount: 0.5
+    // }
+];
 // const dish_list = [
 //     {
-//         id: 'gTgGQ8rF8qo4thFnB',
+//         id: 'RvypCcmYHtT5AMWZz',
 //         amount: 0.5
 //     },
 //     {
-//         id: 'jc4eWDq66ghmogQFC',
+//         id: 'CZDD8M3cyJH8WyXnA',
 //         amount: 0.5
 //     },
 //     {
-//         id: '5D8osuvKbfpCmy9dr',
+//         id: 'FTJhJZqgnDZcwsskn',
+//         amount: 0.5
+//     },
+//     {
+//         id: 'X7NoJSfjDLJxbgGAF',
 //         amount: 0.5
 //     }
 // ];
-const dish_list = [
-    {
-        id: 'RvypCcmYHtT5AMWZz',
-        amount: 0.5
-    },
-    {
-        id: 'CZDD8M3cyJH8WyXnA',
-        amount: 0.5
-    },
-    {
-        id: 'FTJhJZqgnDZcwsskn',
-        amount: 0.5
-    },
-    {
-        id: 'X7NoJSfjDLJxbgGAF',
-        amount: 0.5
-    }
-];
 
 export function get_promotion_list(dish_id) {
     return dish_list;
