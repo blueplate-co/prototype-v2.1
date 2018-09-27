d9d6c0b3661424df03a0732c174e104950192d9d
Tan Pham <tan.pham@blueplate.co>
2018-09-27 11:30:41 +0700

    update functions check cookies in browser

--- imports/functions/common/promotion_common.js
+++ imports/functions/common/promotion_common.js
@@ -55,23 +55,27 @@ export function get_amount_promotion(dish_id) {
 
 export function getCookie(name) {
     var dc = document.cookie;
-    var prefix = name + "=";
-    var begin = dc.indexOf("; " + prefix);
-    if (begin == -1) {
-        begin = dc.indexOf(prefix);
-        if (begin != 0) return null;
-    }
-    else
-    {
-        begin += 2;
-        var end = document.cookie.indexOf(";", begin);
-        if (end == -1) {
-            end = dc.length;
+    if (dc.length > 0) {
+        //- has cookies
+        var prefix = name + "=";
+        var begin = dc.indexOf("; " + prefix);
+        if (begin == -1) {
+            //- no promotion cookies
+            begin = dc.indexOf(prefix);
+            if (begin != 0) return -1;
+        } else {
+            //- has promotion cookies
+            begin += 2;
+            var end = document.cookie.indexOf(";", begin);
+            if (end == -1) {
+                end = dc.length;
+            }
+            return end; 
         }
+    } else {
+        //- no cookies in browser
+        return -1;
     }
-    // because unescape has been deprecated, replaced with decodeURI
-    //return unescape(dc.substring(begin + prefix.length, end));
-    return decodeURI(dc.substring(begin + prefix.length, end));
 }
 
 export function delete_cookies(name) {
