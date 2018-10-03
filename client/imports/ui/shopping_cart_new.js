4d9ca21f219e700a0dc98dac5d42e0442906c15c
ThePhan <the.phan@blueplate.co>
2018-10-03 12:11:02 +0700

    Clean code

--- client/imports/ui/shopping_cart_new.js
+++ client/imports/ui/shopping_cart_new.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class ShoppingCard extends Component {
+export default class ShoppingCart extends Component {
     constructor(props) {
         super(props);
 
