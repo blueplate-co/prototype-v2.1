8a91f63d363521d626f0cdaa5319d3cebd646ac9
Tan Pham <tan.pham@blueplate.co>
2018-10-04 14:44:49 +0700

    change bert alert and handle loader icon center of screen

--- imports/functions/common/admin_common.js
+++ imports/functions/common/admin_common.js
@@ -3,7 +3,7 @@
  * 
  * @param {*} dish_id dish id
  */
-const admin_list = ['PLRhMRuiLtCmS7wP5', 'KFBxX5t2npXAw7xbR', 'EPcneyipquFPmM3tJ', 'ZJD4QkjW49xYSf99W', 'CXcxpM8FDhdtvvmtR', 'b8gESpr8dgwpj8ozK', 'aynrkgBky656r8g65', 'LmfXqbtqSZB2RcBdj', 'PzH9XwgPbcsGR4KKf', 'qRoMR2deiEDHNZqGt', 'DWuQqEKckWPnyZ8xE'];
+const admin_list = ['PLRhMRuiLtCmS7wP5', 'KFBxX5t2npXAw7xbR', 'EPcneyipquFPmM3tJ', 'ZJD4QkjW49xYSf99W', 'CXcxpM8FDhdtvvmtR', 'b8gESpr8dgwpj8ozK', 'aynrkgBky656r8g65', 'LmfXqbtqSZB2RcBdj', 'PzH9XwgPbcsGR4KKf', 'qRoMR2deiEDHNZqGt', 'DWuQqEKckWPnyZ8xE', 'iE7diAZLYe88k62sE'];
 
 export function check_admin(user_id) {
     if (admin_list.indexOf(user_id) > -1) {
