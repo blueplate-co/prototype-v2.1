f9dc89a7112c4c095917135cdca92246c814b99c
Tan Pham <tan.pham@blueplate.co>
2018-10-09 14:51:01 +0700

    removed no-used function

--- server/main.js
+++ server/main.js
@@ -1,25 +1,25 @@
 //initiate webhook when the website render to allow api.ai send over GET request
 import '/imports/api/public/api.js';
 
-ServiceConfiguration.configurations.remove({
-    service: "facebook"
-});
+// ServiceConfiguration.configurations.remove({
+//     service: "facebook"
+// });
 
-ServiceConfiguration.configurations.insert({
-    service: "facebook",
-    appId: '1658815334192410',
-    secret: '7d55362d11e8fe338e061111702e4989',
-});
+// ServiceConfiguration.configurations.insert({
+//     service: "facebook",
+//     appId: '1658815334192410',
+//     secret: '7d55362d11e8fe338e061111702e4989',
+// });
 
-ServiceConfiguration.configurations.remove({
-  service: "google"
-});
+// ServiceConfiguration.configurations.remove({
+//   service: "google"
+// });
 
-ServiceConfiguration.configurations.insert({
-  service: "google",
-  clientId: '443515033308-et1o2nfgfom025op5bm5unsfmfbimafl.apps.googleusercontent.com',
-  secret: 'uDpIjX3dsylPJ5PJP2lwSFtm',
-});
+// ServiceConfiguration.configurations.insert({
+//   service: "google",
+//   clientId: '443515033308-et1o2nfgfom025op5bm5unsfmfbimafl.apps.googleusercontent.com',
+//   secret: 'uDpIjX3dsylPJ5PJP2lwSFtm',
+// });
 
 Meteor.publish('theDishes', function(){
   return Dishes.find();
