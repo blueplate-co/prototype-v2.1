a74f3074a02ecedd06ee6f5a44e25c7a711f6420
Tan Pham <tan.pham@blueplate.co>
2018-10-10 10:04:16 +0700

    change the promotion popup

--- server/main.js
+++ server/main.js
@@ -1,5 +1,5 @@
 //initiate webhook when the website render to allow api.ai send over GET request
-import '/imports/api/public/api.js';
+// import '/imports/api/public/api.js';
 
 // ServiceConfiguration.configurations.remove({
 //     service: "facebook"
