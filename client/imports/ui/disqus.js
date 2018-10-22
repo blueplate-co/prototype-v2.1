e4d712cb88b592203a52daa51f20618fa200d105
Tan Pham <tan.pham@blueplate.co>
2018-10-22 15:32:29 +0700

    reset disqus when it's already loaded

--- client/imports/ui/disqus.js
+++ client/imports/ui/disqus.js
@@ -6,23 +6,27 @@ export default class DisqusComment extends Component {
     }
 
     componentDidMount() {
-            var disqus_config = function () {
-                this.page.url = this.props.url;
-                this.page.identifier = this.props.page;
-            };
-            (function() { // DON'T EDIT BELOW THIS LINE
-                var d = document, s = d.createElement('script');
-                s.src = 'https://blueplate-2.disqus.com/embed.js';
-                s.setAttribute('data-timestamp', +new Date());
-                (d.head || d.body).appendChild(s);
-            })();
-            // DISQUS.reset({
-            //     reload: true,
-            //     config: function () {  
-            //       this.page.identifier = 'dish_' + FlowRouter.getParam("dish_id");  
-            //       this.page.url = window.location.href;
-            //     }
-            // });
+            if (typeof window.DISQUS === 'undefined') {
+                var disqus_config = function () {
+                    this.page.url = this.props.url;
+                    this.page.identifier = this.props.page;
+                };
+                (function() { // DON'T EDIT BELOW THIS LINE
+                    var d = document, s = d.createElement('script');
+                    s.src = 'https://blueplate-2.disqus.com/embed.js';
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                })();
+            } else {
+                DISQUS.reset({
+                    reload: true,
+                    config: function () {  
+                        this.page.url = this.props.url;
+                        this.page.identifier = this.props.page;
+
+                    }
+                });
+            }
     }
 
     render() {
