4bca6e9ec3b7ff50e1ec761dec6eb193ebaea91f
Tan Pham <tan.pham@blueplate.co>
2018-10-23 18:50:43 +0700

    style for halloween

--- client/Components/promotion_modal/promotion_modal.js
+++ client/Components/promotion_modal/promotion_modal.js
@@ -6,5 +6,13 @@ Template.promotion_modal.helpers({
         if (!promotion) promotion = '0';
         var amount = parseInt(promotion.replace(/^\D+/g, ''));
         return amount;
+    },
+    'custom_theme': function() {
+        let expire_day = moment('03/11/2018', 'DD/MM/YYYY');
+        if (expire_day > moment()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 });
