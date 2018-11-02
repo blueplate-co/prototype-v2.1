1576943a606f27b6a84f2b8554efa0be35dd69f3
Tan Pham <tan.pham@blueplate.co>
2018-11-02 17:50:33 +0700

    complete dynamic import dish detail

--- client/Components/dish_detail/dish_detail.js
+++ client/Components/dish_detail/dish_detail.js
@@ -1,14 +1,14 @@
-import { Template } from 'meteor/templating';
-import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
+// import { Template } from 'meteor/templating';
+// import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 
-// integrate reactjs
-import React from 'react';
-import { render } from 'react-dom';
+// // integrate reactjs
+// import React from 'react';
+// import { render } from 'react-dom';
 
-// import for payment react component
-import Dish_Detail from '../../imports/ui/dish_detail';
+// // import for payment react component
+// import Dish_Detail from '../../imports/ui/dish_detail';
 
-Template.dish_detail.onRendered(function(){
-    var id = FlowRouter.getParam("dish_id")
-    render(<Dish_Detail id={id}/>, document.getElementById('dish_detail_container'));
-});
+// Template.dish_detail.onRendered(function(){
+//     var id = FlowRouter.getParam("dish_id")
+//     render(<Dish_Detail id={id}/>, document.getElementById('dish_detail_container'));
+// });
